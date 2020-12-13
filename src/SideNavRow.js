24f8e2b06b57791e446fda45c6123c98c17f78c0
ZakirBangash <66157395+ZakirBangash@users.noreply.github.com>
2020-12-13 15:55:58 +0500

    Slack

--- src/SideNavRow.js
+++ src/SideNavRow.js
@@ -1,13 +1,22 @@
-import React from 'react'
-import './SideNavRow.css'
+import React, { useEffect } from "react";
+import "./SideNavRow.css";
 
-const SideNavRow = ({Icon,title}) => {
-    return (
-        <div className="sideNavRow">
-            {Icon && <Icon className="sideNavRow__icon" />}
-    {Icon ? <h5 >{title}</h5> : <h5 className="sideNavRow__channel"><span className="sideNavRow__hash">#</span> {title}</h5>}
-        </div>
-    )
-}
+const SideNavRow = ({ Icon, title }) => {
+  return (
+    <div className="sideNavRow">
+      {/* {Icon && <Icon className="sideNavRow__icon" />} */}
+      {Icon ? (
+          <>
+          <Icon className="sideNavRow__icon" />
+        <h5>{title}</h5>
+        </>
+      ) : (
+        <h5 className="sideNavRow__channel">
+          <span className="sideNavRow__hash">#</span> {title}
+        </h5>
+      )}
+    </div>
+  );
+};
 
-export default SideNavRow
+export default SideNavRow;
