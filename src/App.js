3cc596f933e5be170098dbfe1c42a469788b020b
ZakirBangash <66157395+ZakirBangash@users.noreply.github.com>
2020-12-15 00:55:47 +0500

    there is bug

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import SideNav from "./SideNav";
 import { Routes, Route, Link } from "react-router-dom";
 import { Chat } from "./Chat";
+import Login from "./Login";
+import { useStateValue } from "./GlobalState";
+
+
 
 function App() {
+  
+  const [{ user }, dispatch] = useStateValue();
+    console.log(user)
+
   return (
     <div className="app">
-      <Header />
-        <div className="app__body">
-          <SideNav />
-         
-      <Routes>
-        
-          <Route
-          path="Room/:slug"
-          element={
-            <>
-                <Chat />
-              
-            </>
-          }
-        />
-       
-      </Routes>
-      </div>
+      {user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
+          <div className="app__body">
+            <SideNav />
+            <Routes>
+              <Route
+                path="Room/:slug"
+                element={
+                  <>
+                    <Chat />
+                  </>
+                }
+              />
+            </Routes>
+          </div>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Login.css'
+import Button from '@material-ui/core/Button';
+import {auth, provider} from './firebase'
+
+const Login = () => {
+
+const signIN = () => {
+    auth.signInWithPopup(provider).then(function(result) {
+        console.log(result)
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorMessage)
+        // The email of the user's account used.
+        var email = error.email;
+      
+      });
+}
+
+    return (
+        <div className="login">
+            <div className="login__container">
+                <img src="https://a.slack-edge.com/bv1-8/slack_logo-ebd02d1.svg" alt="slack"/>
+                <h1>Sign in to Slack</h1>    
+                <p>Continue with the Google account or email address you use to sign in.</p>    
+                <Button variant="contained" color="primary" onClick={signIN}>
+                Continue with the Google account
+               </Button>        
+            </div>
+        </div>
+    )
+}
+
+export default Login
